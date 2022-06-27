a39acbef95a77f91288bdca36639f2f36fe411b5
jpnm-panda <jpnm0415shkm@gmail.com>
2022-06-28 08:10:21 +0900

    staticを確認

--- typescriot-lesson/src/class.ts
+++ typescriot-lesson/src/class.ts
@@ -9,25 +9,31 @@ class Person {
   //   this.name = initName;
   //   this.age = initAge;
 
-  // readonly をつけると値の書き換えができなくなる
-  readonly id: number = 31;
-
   // // class の初期化部分は省略できる
   // constructor(public readonly name: string, private age: number) {
   //   // constructor メソッドの中だと書き換えはできる
   //   this.id = 32;
   // }
 
+  // this: を設定することで、greeting を呼び出したobject にname がるか確認できる
+  // greeting(this: { name: string }) {
+  //   console.log(`Hello! My name is ${this.name} !!`);
+  // }
+
+  static species = "Homo sapiens";
+  static isAdult(age: number) {
+    if (age > 17) return true;
+    return false;
+  }
+
+  // readonly をつけると値の書き換えができなくなる
+  readonly id: number = 31;
+
   // private 修飾子の場合、継承先のclass でも値を参照できなくなるが、protected なら参照可能
   constructor(public readonly name: string, protected age: number) {
     // constructor メソッドの中だと書き換えはできる
     this.id = 32;
   }
-
-  // this: を設定することで、greeting を呼び出したobject にname がるか確認できる
-  // greeting(this: { name: string }) {
-  //   console.log(`Hello! My name is ${this.name} !!`);
-  // }
 
   incrementAge() {
     this.age += 1;
@@ -88,3 +94,8 @@ console.log("teacher.subject", teacher.subject);
 teacher.subject = "Music";
 console.log("teacher.subject", teacher.subject);
 teacher.greeting();
+
+console.log(Person.species);
+console.log(Person.isAdult(27));
+console.log(Teacher.species);
+console.log(Teacher.isAdult(38));
