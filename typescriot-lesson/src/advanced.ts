891a41f5c699c3be965e7e279d2bb9a77f39974e
jpnm-panda <jpnm0415shkm@gmail.com>
2022-07-02 10:25:05 +0900

    オーバーロードをやった

--- typescriot-lesson/src/advanced.ts
+++ typescriot-lesson/src/advanced.ts
@@ -37,11 +37,17 @@ type NumberBoolean = number | boolean;
 type StringNumber = string | number;
 type Mix = NumberBoolean & StringNumber;
 
-const toUpperCase = (x: string | number) => {
-  if (typeof x === "string") return x.toUpperCase;
+// 関数の引数と返り値の形が複数ある場合は場合分けをリストかすることで、実行時の型を正確に定義できる。これを関数のオーバーロードという。
+function toUpperCase(x: string): string;
+function toUpperCase(x: number): number;
+// オーバーロードを使うと実装部分で書いた型注釈は無いことになるので、要注意（ここは明示的に書いてるけど型注釈は効いてない）
+function toUpperCase(x: string | number) {
+  if (typeof x === "string") return x.toUpperCase();
 
-  return ";";
-};
+  return x;
+}
+
+const upperHello = toUpperCase("Hello");
 
 type NomadWorker = Engineer | Blogger;
 
@@ -87,3 +93,17 @@ havePet(new Bird());
 // const input = <HTMLInputElement>document.getElementById("input");
 const input = document.getElementById("input") as HTMLInputElement;
 input.value = "initial input value";
+
+// インデックスシグネチャーを使うと、プロパティの追加ができるようになるが、設定した型がプロパティの値全てに反映されるので要注意（ここだとstring）。
+interface Designer {
+  name: string;
+  [index: string]: string;
+}
+
+const designer: Designer = {
+  name: "Junua",
+  role: "web",
+};
+console.log(designer.role);
+// オブジェクトのプロパティは何を指定してもエラーがでなくなる
+console.log(designer.hoge);
