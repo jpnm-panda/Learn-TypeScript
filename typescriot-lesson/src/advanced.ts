f183b30cbce21715ffd09b9f28174efb3b9d25f4
jpnm-panda <jpnm0415shkm@gmail.com>
2022-07-02 09:14:21 +0900

    型ガードを学習

---
+++ typescriot-lesson/src/advanced.ts
@@ -0,0 +1,79 @@
+// type Engineer = {
+//   name: string;
+//   role: string;
+// }
+
+// type Blogger = {
+//   name: string
+//   follower: number;
+// }
+
+// type EngineerBlogger = Engineer & Blogger;
+// const Junya: EngineerBlogger = {
+//   name: 'junya',
+//   role: 'front-end',
+//   follower: 1000
+// }
+
+interface Engineer {
+  name: string;
+  role: string;
+}
+
+interface Blogger {
+  name: string;
+  follower: number;
+}
+
+interface EngineerBlogger extends Engineer, Blogger {}
+
+const Junya: EngineerBlogger = {
+  name: "junya",
+  role: "front-end",
+  follower: 1000,
+};
+
+type NumberBoolean = number | boolean;
+type StringNumber = string | number;
+type Mix = NumberBoolean & StringNumber;
+
+const toUpperCase = (x: string | number) => {
+  if (typeof x === "string") return x.toUpperCase;
+
+  return ";";
+};
+
+type NomadWorker = Engineer | Blogger;
+
+function describeProfile(nomadWorker: NomadWorker) {
+  console.log(nomadWorker.name);
+
+  if ("role" in nomadWorker) console.log(nomadWorker.role);
+
+  if ("follower" in nomadWorker) console.log(nomadWorker.follower);
+}
+
+class Dog {
+  speak() {
+    console.log("bow-bow");
+  }
+}
+
+class Bird {
+  speak() {
+    console.log("tweet-tweet");
+  }
+  fly() {
+    console.log("flutter");
+  }
+}
+
+type Pet = Dog | Bird;
+
+// ここのPet をinterface にしてしまうとエラーになる。interface はコンパイル後に残らないため。
+const havePet = (pet: Pet) => {
+  pet.speak();
+  if (pet instanceof Bird) pet.fly();
+};
+
+havePet(new Bird());
